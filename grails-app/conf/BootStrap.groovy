c1092221bc77521c8b54507fec5b7a0bfa7e85ca
Jon Reckner <jreckner@lexmark.com>
2013-03-06 17:40:50 -0500

    add initial Grails app

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import org.apache.shiro.crypto.hash.Sha256Hash
+import com.wireblend.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        def user = new User(username: "bang", passwordHash: new Sha256Hash("bang").toHex())
+        user.addToPermissions("*:*")
+        user.save()
     }
     def destroy = {
     }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,5 +59,7 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.2.1"
 
         compile ':cache:1.0.1'
+
+        compile ":shiro:1.1.4"
     }
 }
