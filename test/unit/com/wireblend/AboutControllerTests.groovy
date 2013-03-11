95cfeb7602d3a5b05034894c6975fed5ae912796
Matt Rust <mrust@lexmark.com>
2013-03-10 20:58:32 -0400

    Adding an About Controller and beefing up registration page.

---
+++ test/unit/com/wireblend/AboutControllerTests.groovy
@@ -0,0 +1,18 @@
+package com.wireblend
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AboutController)
+class AboutControllerTests {
+
+    void testSomething() {
+//        fail "Implement me"
+        assert true
+    }
+}
